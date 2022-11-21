a591edb656559c169f47fb1c5b32ad2686887f4b
Sergio Reyes <sergio.reyes02@utrgv.edu>
2022-11-21 13:31:12 -0600

    add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int64_value,bool_value,string_value)VALUES (0,1,'oh look its true');
+
+--UPDATE sandbox SET string_value = 'THE'WHERE id = 49;
+
+--DELETE FROM sandbox WHERE id = 3;
